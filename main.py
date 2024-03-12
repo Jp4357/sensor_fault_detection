f2292930d7a9da4238fd5f5d07a5ec39c4d82145
Jp4357 <jyotiprakash4357@gmail.com>
2024-03-12 16:16:44 +0530

    added data ingestion config

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from sensor.exception import SensorException
 from sensor.logger import logging
+from sensor.utils import get_collection_as_dataframe
 import sys,os
+from sensor.entity import config_entity
 
 
 
      
 if __name__ == '__main__':
      try:
-          pass
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+          print(data_ingestion_config.to_dict())
      except Exception as e:
           print(e)
